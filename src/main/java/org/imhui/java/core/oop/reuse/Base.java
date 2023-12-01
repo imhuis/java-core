8c8a1274dcf828fb3a63a7c980be3e32c08c0df9
imhuis <yjh19980105@gmail.com>
2023-12-01 22:50:57 +0800

    feat: update code

---
+++ src/main/java/org/imhui/java/core/oop/reuse/Base.java
@@ -0,0 +1,9 @@
+package org.imhui.java.core.oop.reuse;
+
+/**
+ * @author: imhuis
+ * @date: 2023/12/1
+ * @description:
+ */
+public class Base {
+}
---
+++ src/main/java/org/imhui/java/core/oop/reuse/BaseWithEmptyConstructor.java
@@ -0,0 +1,13 @@
+package org.imhui.java.core.oop.reuse;
+
+/**
+ * @author: imhuis
+ * @date: 2023/12/1
+ * @description:
+ */
+public class BaseWithEmptyConstructor {
+    private String s;
+    public BaseWithEmptyConstructor(String s) {
+        this.s = s;
+    }
+}
