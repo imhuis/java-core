7ade23d51a59bf689f64bc1f3ba90680179b5120
imhuis <yjh19980105@gmail.com>
2023-12-09 22:37:55 +0800

    feat: update code

--- src/main/java/org/imhui/java/core/collections/map/MapOps.java
+++ src/main/java/org/imhui/java/core/collections/map/MapOps.java
@@ -7,15 +7,27 @@ import java.util.Map;
  * @date: 2023/11/24
  * @description:
  */
+
+record A(String name) {
+
+}
+
+record B(String name) {
+
+}
 public class MapOps {
 
     static Map<String, String> map;
 
+
+
     public static void main(String[] args) {
-        map.entrySet().stream()
-                .map(Map.Entry::getValue)
-                .filter(v -> v.startsWith("Dark"))
-                .map(v -> v.replaceFirst("Dark", "Light"))
-                .forEach(System.out::println);
+//        map.entrySet().stream()
+//                .map(Map.Entry::getValue)
+//                .filter(v -> v.startsWith("Dark"))
+//                .map(v -> v.replaceFirst("Dark", "Light"))
+//                .forEach(System.out::println);
+
+        Map.of(new A("a"), "b", new B("b"), "d");
     }
 }
---
+++ src/main/java/org/imhui/java/core/feature/README.md
@@ -0,0 +1,7 @@
+# java new feature
+
+## java16
+
+### record
+
+Nested record is static
